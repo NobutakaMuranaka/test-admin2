aafd3fe103094f9604f888debef513349510658e
EC2 Default User <ec2-user@ip-172-31-12-3.us-east-2.compute.internal>
2021-09-17 12:50:10 +0000

    ActiveAdminのインストール

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,11 @@ module TestAdmin2
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+   config.load_defaults 5.2
+
+   config.generators do |g|
+     g.assets false          # CSS, JavaScriptファイルは生成しない
+     g.test_framework false  # testファイルは生成しない
+   end
   end
 end
